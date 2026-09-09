e36d0c128d7c6bf3037f200e6273060d589cc49f
garibe1123 <rlaalfk1013@gmail.com>
2026-09-10 01:27:39 +0900

    Keep legacy number input disabled for combat router

--- Assets/Script/BattleRework/UI/BattleEquipmentGridAutoInstaller.cs
+++ Assets/Script/BattleRework/UI/BattleEquipmentGridAutoInstaller.cs
@@ -42,10 +42,10 @@ public static class BattleEquipmentGridAutoInstaller
             BattleEquipmentSystem equipment = manager.GetComponent<BattleEquipmentSystem>();
             if (equipment != null)
             {
-                if (!equipment.LegacyNumberKeyEquipEnabled)
+                if (equipment.LegacyNumberKeyEquipEnabled)
                 {
-                    Undo.RecordObject(equipment, "Enable Number Key Equipment Input");
-                    equipment.LegacyNumberKeyEquipEnabled = true;
+                    Undo.RecordObject(equipment, "Disable Legacy Number Key Equipment Input");
+                    equipment.LegacyNumberKeyEquipEnabled = false;
                     changed = true;
                 }
 
@@ -90,7 +90,7 @@ public static class BattleEquipmentGridAutoInstaller
             BattleEquipmentSystem equipment = manager.GetComponent<BattleEquipmentSystem>();
             if (equipment != null)
             {
-                equipment.LegacyNumberKeyEquipEnabled = true;
+                equipment.LegacyNumberKeyEquipEnabled = false;
                 if (equipment.UnlockedSlotCount < MinimumBackpackSlots) equipment.SetUnlockedSlotCount(MinimumBackpackSlots);
             }
 
