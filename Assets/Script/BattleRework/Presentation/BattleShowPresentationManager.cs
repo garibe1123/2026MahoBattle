4d8871fbc0d38e3bf8e0df2bad3dcd5fcf6d50c3
garibe1123 <rlaalfk1013@gmail.com>
2026-09-25 05:32:55 +0900

    split selection and combat speech tail styles

--- Assets/Script/BattleRework/Presentation/BattleShowPresentationManager.cs
+++ Assets/Script/BattleRework/Presentation/BattleShowPresentationManager.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Serialization;
 
 
 public enum ScreenPresenterMotionState
@@ -166,9 +167,14 @@ public sealed class BattleShowPresentationManager : MonoBehaviour
     [Tooltip("Reward/Map 큰 사회자와 전투 리액션 상체 사회자가 함께 사용하는 Shader Material입니다. 비어 있으면 기본 UI Material을 사용합니다.")]
     [SerializeField] private Material screenPresenterMaterial;
 
-    [Header("말풍선 꼬리 스타일")]
-    [Tooltip("전투/아이템/맵 선택이 공통으로 사용하는 코드 생성 말풍선 꼬리 프로파일입니다. Pivot/폭/Stroke를 수정하면 Editor Preview와 런타임에 동일하게 반영됩니다.")]
-    [SerializeField] private BattleSpeechBubbleTailStyle speechBubbleTailStyle = new();
+    [Header("선택씬 말풍선 꼬리 스타일")]
+    [Tooltip("Reward / 아이템 선택 / 맵 선택에서 사용하는 코드 생성 말풍선 꼬리 프로파일입니다.")]
+    [FormerlySerializedAs("speechBubbleTailStyle")]
+    [SerializeField] private BattleSpeechBubbleTailStyle selectionSpeechBubbleTailStyle = new();
+
+    [Header("전투씬 말풍선 꼬리 스타일")]
+    [Tooltip("전투 중 우측 상단 사회자 리액션 말풍선에서만 사용하는 별도 꼬리 프로파일입니다.")]
+    [SerializeField] private BattleSpeechBubbleTailStyle combatSpeechBubbleTailStyle = new();
 
     [Header("버드아이뷰 조명 Sprite Sheet")]
     [Tooltip("플레이어와 사회자 발밑 조명에 사용할 Sprite Sheet 프레임입니다. 비어 있으면 BattleHUD가 만드는 기본 타원형 조명을 사용합니다.")]
@@ -233,8 +239,11 @@ public sealed class BattleShowPresentationManager : MonoBehaviour
 
     public Material ScreenPresenterMaterial => screenPresenterMaterial;
 
-    public BattleSpeechBubbleTailStyle SpeechBubbleTailStyle =>
-        speechBubbleTailStyle ??= new BattleSpeechBubbleTailStyle();
+    public BattleSpeechBubbleTailStyle SelectionSpeechBubbleTailStyle =>
+        selectionSpeechBubbleTailStyle ??= new BattleSpeechBubbleTailStyle();
+
+    public BattleSpeechBubbleTailStyle CombatSpeechBubbleTailStyle =>
+        combatSpeechBubbleTailStyle ??= new BattleSpeechBubbleTailStyle();
 
     public ScreenPresenterMotionClip GetScreenPresenterMotion(ScreenPresenterMotionState state)
     {
